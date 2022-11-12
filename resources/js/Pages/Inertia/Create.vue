d50cbf8b29e1b606fc128f8aa47e5f13ab62ff58
hide <majihaik@gmail.com>
2022-11-12 18:02:31 +0900

    テストルート設置、フォーム

---
+++ resources/js/Pages/Inertia/Create.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { reactive } from 'vue';
+import { Inertia } from '@inertiajs/inertia';
+
+const form = reactive({
+    title: null,
+    content: null,
+})
+
+defineProps({
+    errors:Object
+})
+
+const submitFunction = ()=> {
+    Inertia.post('/inertia', form)
+}
+</script>
+
+
+<template>
+    <form @submit.prevent="submitFunction">
+        <div>タイトル</div>
+        <input name="title" type="text" v-model="form.title"> <br>
+        <div v-if="errors.title">{{errors.title}}</div>
+        <div>コンテンツ</div>
+        <input name="content" type="text" v-model="form.content">
+        <div v-if="errors.content">{{errors.content}}</div>
+        <div>
+            <button type="submit">送信</button>
+        </div>
+    </form>
+</template>
+
+
---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+import {ref} from 'vue';
+
+const newTitle = ref('');
+const newContent = ref('');
+
+</script>
+
+<template>
+    <Link :href="route('inertia.index')">名前付きルートの確認です　</Link><br>
+    <Link :href="route('inertia.show', { id: 1 })">botann</Link>
+
+    <div class="mb-90"></div>
+    <div>
+        title
+    </div>
+    <input type="text" name="newTitle" v-model="newTitle">{{newTitle}}<br>
+    <div>
+        content
+    </div>
+    <input type="text" name="newContent" v-model="newContent">{{newContent}}<br>
+    <Link method="post" as="button" :href="route('inertia.store')" :data="{
+        title: newTitle,
+        content: newContent
+    }">DB保存テスト</Link>
+</template>
