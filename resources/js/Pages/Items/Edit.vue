5c3d3be2ba59b3adc22f5fe6653b6d064089e4c5
hide <majihaik@gmail.com>
2022-11-21 15:08:28 +0900

    Item Update フロント側のバリテーション未実装

--- resources/js/Pages/Items/Edit.vue
+++ resources/js/Pages/Items/Edit.vue
@@ -4,20 +4,24 @@ import { Head } from '@inertiajs/inertia-vue3';
 import { reactive } from 'vue';
 import { Inertia } from '@inertiajs/inertia';
 import InputError from '@/Components/InputError.vue';
+import { objectToString } from '@vue/shared';
 
+const props = defineProps({
+    item: Object,
+})
 
 const form = reactive({
-    name: null,
-    memo: null,
-    price: null
+    id: props.item.id,
+    name: props.item.name,
+    memo: props.item.memo,
+    price: props.item.price,
+    is_selling: props.item.is_selling
 })
 
-defineProps({
-    errors: Object
-})
 
-const storeItem = () => {
-    Inertia.post('/items', form)
+
+const updateItem = id => {
+    Inertia.put(route('items.update', {item: id}), form)
 }
 </script>
 
@@ -28,7 +32,7 @@ const storeItem = () => {
     <AuthenticatedLayout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                商品登録
+                商品編集
             </h2>
         </template>
 
@@ -37,7 +41,7 @@ const storeItem = () => {
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <section class="text-gray-600 body-font relative">
-                            <form @submit.prevent="storeItem">
+                            <form @submit.prevent="updateItem(form.id)">
                                 <div class="container px-5 py-8 mx-auto">
                                     <div class="lg:w-1/2 md:w-2/3 mx-auto">
                                         <div class="-m-2">
@@ -48,7 +52,6 @@ const storeItem = () => {
                                                     <input type="text" id="name" name="name" v-model="form.name"
                                                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                 </div>
-                                                <div v-if="errors.name" class="text-red-600">{{ errors.name }}</div>
                                             </div>
                                             <div class="p-2 w-full">
                                                 <div class="relative">
@@ -56,7 +59,6 @@ const storeItem = () => {
                                                     <textarea id="memo" name="memo" v-model="form.memo"
                                                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                                 </div>
-                                                <div v-if="errors.memo" class="text-red-600">{{ errors.memo }}</div>
                                             </div>
                                             <div class="p-2 w-full">
                                                 <div class="relative">
@@ -65,12 +67,20 @@ const storeItem = () => {
                                                     <input type="number" id="price" name="price" v-model="form.price"
                                                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                 </div>
-                                                <div v-if="errors.price" class="text-red-600">{{ errors.price }}</div>
+                                            </div>
+                                            <div class="p-2 w-full">
+                                                <div class="relative">
+                                                    <label for="is_selling"
+                                                        class="leading-7 text-sm text-gray-600 mr-2">ステータス</label>
+                                                    <input type="radio" id="is_selling" name="is_selling" v-model="form.is_selling" value="1">
+                                                    <label class="ml-2 mr-2 text-center">販売中</label>
+                                                    <input type="radio" id="is_selling" name="is_selling" v-model="form.is_selling" value="2">
+                                                    <label class="ml-2 mr-2 text-center">停止中</label>
+                                                </div>
                                             </div>
                                         </div>
                                         <div class="p-2 w-full">
-                                            <button
-                                                class="flex mx-auto text-white bg-indigo-500 border-0 py-3 px-3 focus:outline-none hover:bg-indigo-600 rounded text-lg">商品登録</button>
+                                            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-3 px-3 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
                                         </div>
                                     </div>
                                 </div>
--- resources/js/Pages/Items/Show.vue
+++ resources/js/Pages/Items/Show.vue
@@ -1,7 +1,8 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/inertia-vue3';
-import {nl2br} from '@/common';
+import { Head, Link } from '@inertiajs/inertia-vue3';
+import { nl2br } from '@/common';
+// import route from 'vendor/tightenco/ziggy/src/js';
 
 
 defineProps({
@@ -28,54 +29,56 @@ const storeItem = () => {
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <section class="text-gray-600 body-font relative">
-                            <form @submit.prevent="storeItem">
-                                <div class="container px-5 py-8 mx-auto">
-                                    <div class="lg:w-1/2 md:w-2/3 mx-auto">
-                                        <div class="-m-2">
+                            <div class="container px-5 py-8 mx-auto">
+                                <div class="lg:w-1/2 md:w-2/3 mx-auto">
+                                    <div class="-m-2">
+                                        <div class="p-2 w-full">
+                                            <div class="relative">
+                                                <label for="name" class="leading-7 text-sm text-gray-600">商品名</label>
+                                                <div
+                                                    class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                    {{ item.name }}
+                                                </div>
+                                            </div>
                                             <div class="p-2 w-full">
                                                 <div class="relative">
-                                                    <label for="name"
-                                                        class="leading-7 text-sm text-gray-600">商品名</label>
-                                                    <div
-                                                        class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                                        {{ item.name }}
-                                                    </div>
-                                                </div>
-                                                <div class="p-2 w-full">
-                                                    <div class="relative">
-                                                        <label for="memo"
-                                                            class="leading-7 text-sm text-gray-600">メモ</label>
-                                                        <div id="memo" class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
-                                                            {{ item.memo }}
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="p-2 w-full">
-                                                    <div class="relative">
-                                                        <label for="price"
-                                                            class="leading-7 text-sm text-gray-600">商品価格</label>
-                                                        <div id="price"
-                                                            class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                                            {{item.price}}
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="p-2 w-full">
-                                                    <div class="relative">
-                                                        <label for="price"
-                                                            class="leading-7 text-sm text-gray-600">ステータス</label>
-                                                        <div id="price"
-                                                            class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                                            <span v-if="item.is_selling === 1">販売中</span>
-                                                            <span v-if="item.is_selling === 2">停止中</span>
-                                                        </div>
+                                                    <label for="memo" class="leading-7 text-sm text-gray-600">メモ</label>
+                                                    <div id="memo"
+                                                        class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
+                                                        {{ item.memo }}
                                                     </div>
                                                 </div>
                                             </div>
+                                            <div class="p-2 w-full">
+                                                <div class="relative">
+                                                    <label for="price"
+                                                        class="leading-7 text-sm text-gray-600">商品価格</label>
+                                                    <div id="price"
+                                                        class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                        {{ item.price }}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="p-2 w-full">
+                                                <div class="relative">
+                                                    <label for="price"
+                                                        class="leading-7 text-sm text-gray-600">ステータス</label>
+                                                    <div id="price"
+                                                        class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                        <span v-if="item.is_selling === 1">販売中</span>
+                                                        <span v-if="item.is_selling === 2">停止中</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="p-2 w-full">
+                                                <Link as="button" :href="route('items.edit', {item: item.id})" class="flex mx-auto text-white bg-indigo-500 px-3 py-3 ont-semibold text-xl  leading-tight">編集</Link>
+                                            </div>
                                         </div>
+
                                     </div>
                                 </div>
-                            </form>
+                            </div>
 
                         </section>
                     </div>
