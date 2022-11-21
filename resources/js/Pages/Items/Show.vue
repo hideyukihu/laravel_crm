fe8fd94b213313bb0e184998a0ebab8b938fff2c
hide <majihaik@gmail.com>
2022-11-21 09:34:40 +0900

    改行未実装

--- resources/js/Pages/Items/Show.vue
+++ resources/js/Pages/Items/Show.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/inertia-vue3';
+import {nl2br} from '@/common';
 
 
 defineProps({
@@ -36,7 +37,7 @@ const storeItem = () => {
                                                     <label for="name"
                                                         class="leading-7 text-sm text-gray-600">商品名</label>
                                                     <div
-                                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                        class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                         {{ item.name }}
                                                     </div>
                                                 </div>
@@ -44,10 +45,9 @@ const storeItem = () => {
                                                     <div class="relative">
                                                         <label for="memo"
                                                             class="leading-7 text-sm text-gray-600">メモ</label>
-                                                        <div id="memo"
-                                                            class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
+                                                        <div id="memo" class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
+                                                            {{ item.memo }}
                                                         </div>
-                                                        {{ item.memo }}
                                                     </div>
                                                 </div>
                                                 <div class="p-2 w-full">
@@ -55,7 +55,7 @@ const storeItem = () => {
                                                         <label for="price"
                                                             class="leading-7 text-sm text-gray-600">商品価格</label>
                                                         <div id="price"
-                                                            class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                            class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                             {{item.price}}
                                                         </div>
                                                     </div>
@@ -65,16 +65,12 @@ const storeItem = () => {
                                                         <label for="price"
                                                             class="leading-7 text-sm text-gray-600">ステータス</label>
                                                         <div id="price"
-                                                            class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                            class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                             <span v-if="item.is_selling === 1">販売中</span>
                                                             <span v-if="item.is_selling === 2">停止中</span>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div class="p-2 w-full">
-                                                <button
-                                                    class="flex mx-auto text-white bg-indigo-500 border-0 py-3 px-3 focus:outline-none hover:bg-indigo-600 rounded text-lg">商品登録</button>
                                             </div>
                                         </div>
                                     </div>
