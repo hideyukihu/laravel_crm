69e355c12807dc907c0bcde955fa2f3d4b0ea47d
hide <majihaik@gmail.com>
2022-11-23 17:23:22 +0900

    Customer 検索機能

--- resources/js/Pages/Customers/Index copy.vue
+++ resources/js/Pages/Customers/Index copy.vue
@@ -63,6 +63,8 @@ defineProps({
 
                                         </tbody>
                                     </table>
+
+                                    
                                 </div>
 
                             </div>
---
+++ resources/js/Pages/Customers/Indexpaginate.vue
@@ -0,0 +1,34 @@
+<script setup>
+import FlashMessage from '@/Components/FlashMessage.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link } from '@inertiajs/inertia-vue3';
+import Pagination from '@/Components/Pagination.vue';
+import { ref } from 'vue';
+import { Inertia, inertia } from '@inertiajs/inertia';
+
+// import { onMounted } from 'vue';
+
+defineProps({
+    customers: Object
+})
+
+const search = ref('');
+
+const searchCustomers = () => {
+    Inertia.get(route('customers.index', {
+        serch: serch.value
+    }))
+}
+</script>
+
+<template>
+
+    
+
+    <ul v-for="customer in customers.data" :key="customer.id">
+        <li>{{ customer.id }}</li>
+        <li>{{ customer.name }}</li>
+    </ul>
+
+    <Pagination class="mt-6" :links="customers.links"></Pagination>
+</template>
